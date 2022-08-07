26c406d5091906e8d825aed1a03c5f8fbfb1abbc
erickseis <erickseis@gmail.com>
2022-08-06 22:54:47 -0400

    carga sabado 06-08-22

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 window.addEventListener('scroll', function(){
-    let animation = document.getElementById('animado');
+    let animation = document.getElementById('animado', 'box-review1');
     let positionOne = animation.getBoundingClientRect().top;
- })
+    let widthScreen = window.innerHeight/5;
+
+if(positionOne < widthScreen){
+    animation.style.animation = 'mover 1s ease-out'
+}
+
+})
