fa83214a36d1aefe1bec0ba3693d6bb7503a9f17
erickseis <erickseis@gmail.com>
2022-08-13 11:56:18 -0400

    3racarga

---
+++ main.js
@@ -0,0 +1,9 @@
+let body = document.querySelector("body")
+let themeIcon = document.getElementById("dark")
+
+themeIcon.addEventListener( "click", (e) =>{
+
+body.classList.toggle("dark-theme")
+        
+})
+
--- src/index.js
+++
@@ -1,9 +0,0 @@
-let body = document.querySelector("body")
-let themeIcon = document.getElementById("dark")
-
-themeIcon.addEventListener( "click", (e) =>{
-
-body.classList.toggle("dark-theme")
-        
-})
-
