854eaf90625e55aca1f1a9faa9d5614f8771b386
erickseis <erickseis@gmail.com>
2022-08-13 22:13:36 -0400

    3racarga

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,3 +7,22 @@ body.classList.toggle("dark-theme")
         
 })
 
+window.sr = ScrollReveal();
+
+    sr.reveal('.description', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.btn-cv', {
+        duration: 3000,
+        origin:'left',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-img', {
+        duration: 3000,
+        origin:'rigth',
+        distance: '-100px'
+    });
+
