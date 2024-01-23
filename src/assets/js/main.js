c63d29de7cfd9ffaa3a68739089a8bba960a4164
erickseis <erickseis@gmail.com>
2024-01-23 16:45:03 -0300

    carga.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,26 @@
 let body = document.querySelector("body")
 let themeIcon = document.getElementById("dark")
+// Comprueba si el modal debe estar oculto al cargar la página
+if (localStorage.getItem('hideModal') === 'true') {
+    document.getElementById('lenguage-modal').classList.add('hidden');
+}
 
+document.getElementById('spanol').addEventListener('click', function (event) {
+    event.preventDefault();
+    localStorage.setItem('hideModal', 'true');  // Guarda el estado del modal
+    window.location.href = "/index.html";
+});
+document.getElementById('portugues-link').addEventListener('click', function (event) {
+    event.preventDefault();
+    localStorage.setItem('hideModal', 'true');  // Guarda el estado del modal
+    window.location.href = "/index.pt.html";
+});
+
+document.getElementById('english-link').addEventListener('click', function (event) {
+    event.preventDefault();
+    localStorage.setItem('hideModal', 'true');  // Guarda el estado del modal
+    window.location.href = "/index.en.html";
+});
 
 themeIcon.addEventListener("click", (e) => {
 
