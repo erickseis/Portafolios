552b28d919d26a1cb9b267cbb142ae03b1042d65
erick seis <erickseis@gmail.com>
2024-03-16 14:36:37 -0300

    cambios recientes portfolio loader, videos

--- public/src/assets/js/main.js
+++ public/src/assets/js/main.js
@@ -1,5 +1,31 @@
 let body = document.querySelector("body")
 let themeIcon = document.getElementById("dark")
+const loader = document.getElementById("loader")
+
+window.addEventListener("DOMContentLoaded", () => {
+    showLoader();
+})
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+
+        hideLoader();
+        ScrollReveal().reveal('.scroll-img');
+        ScrollReveal().reveal('#animado');
+    }, 2000)
+
+})
+
+const showLoader = () => {
+    loader.classList.add("show_loader")
+
+}
+const hideLoader = () => {
+    loader.classList.remove("show_loader")
+    const app = document.getElementById("app")
+    app.classList.add("view-app")
+
+}
 
 themeIcon.addEventListener("click", (e) => {
 
